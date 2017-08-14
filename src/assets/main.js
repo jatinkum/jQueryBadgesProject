f967f80fdc769d93082f3fddf14348cb2dfbb50f
Jatin <jatinkum@buffalo.edu>
2017-08-14 11:28:12 -0400

    Completed assignment

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,37 @@
 $(function() {
 
   // your code will go here
+    $.ajax({
+    url: 'https://www.codeschool.com/users/jatin258.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      // handle response
+      var arrCourseComp =response.courses.completed;
+      var $badges=$('#badges');
+      $.each(arrCourseComp,function(key,value){
+
+      	
+      	var course=$('<div/>',{
+      		'class':'course'
+      	}).appendTo($badges);
+      	
+      	$('<h3 />',{
+      		'text':value.title
+      	}).appendTo(course);
+
+      	$('<img />',{
+      		'src':value.badge
+      	}).appendTo(course);
+
+      	$('<a />',{
+      		href:value.url,
+      		target:"_blank",
+      		text:"See Course",
+      		'class':'btn btn-primary'
+      	}).appendTo(course);
+      	
+      });
+    }
+  });
 
 });
